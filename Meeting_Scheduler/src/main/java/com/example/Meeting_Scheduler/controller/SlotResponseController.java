8044c2e056f5508788701b9726ea707977bb19c8
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-10-23 14:54:34 +0530

    Started Response for thymeleaf connection

--- Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
+++ Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
@@ -1,5 +1,29 @@
 package com.example.Meeting_Scheduler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Meeting_Scheduler.service.MeetingSlotService;
+import com.example.Meeting_Scheduler.service.SlotResponseService;
+
+@Controller
+@RequestMapping("/responses")
 public class SlotResponseController {
+
+    @Autowired
+    SlotResponseService responseService;
+
+    @Autowired
+    MeetingSlotService slotService;
+
+    @GetMapping("/list")
+    public String getAllResponses(Model model) {
+        model.addAttribute("responses", responseService.getAllResponses());
+        return "/responses/list";
+
+    }
 
 }
