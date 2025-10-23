52eb17b8e4c48aa7c76db57b2fef4fd728f5bfe5
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-10-23 15:14:45 +0530

    response form

--- Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
+++ Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Meeting_Scheduler.entity.SlotResponse;
 import com.example.Meeting_Scheduler.service.MeetingSlotService;
 import com.example.Meeting_Scheduler.service.SlotResponseService;
 
@@ -19,11 +22,19 @@ public class SlotResponseController {
     @Autowired
     MeetingSlotService slotService;
 
+    // Display all Responses
     @GetMapping("/list")
     public String getAllResponses(Model model) {
         model.addAttribute("responses", responseService.getAllResponses());
         return "/responses/list";
+    }
 
+    // Form for new response
+    @GetMapping("/form")
+    public String showResponseForm(Model model) {
+        model.addAttribute("responses", new SlotResponse());
+        model.addAttribute("slots", slotService.getAllMeetingSlots());
+        return "/responses/form";
     }
 
 }
