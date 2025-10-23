02e46d3ea22220fb6ac1b82efb180d0ccab4d449
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-10-23 19:05:47 +0530

    save response to db

--- Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
+++ Meeting_Scheduler/src/main/java/com/example/Meeting_Scheduler/controller/SlotResponseController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.Meeting_Scheduler.entity.MeetingSlot;
 import com.example.Meeting_Scheduler.entity.SlotResponse;
 import com.example.Meeting_Scheduler.service.MeetingSlotService;
 import com.example.Meeting_Scheduler.service.SlotResponseService;
@@ -37,4 +40,10 @@ public class SlotResponseController {
         return "/responses/form";
     }
 
+    // Save response to db
+    @PostMapping
+    public String createResponse(@ModelAttribute ("responses")SlotResponse response , @RequestParam("slotId") Long slotId)
+    {
+        MeetingSlot slot = slotService
+    }
 }
